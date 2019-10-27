96bb2cea0f605faf096c84e5d7f148c55a4c975f
Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
2019-10-27 07:37:16 +0700

    Update dropdown

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    // jQuery('.main-menu .mega-menu-tree').parent('ul').addClass('mega-menu three-column');
-
     $('[data-toggle="tooltip"]').tooltip();
 
     /*---stickey menu---*/
