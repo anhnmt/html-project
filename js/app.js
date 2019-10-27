6e13d4aef064e35f06c4b18fb16defd3a3167292
Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
2019-10-28 00:05:36 +0700

    Update slider

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     'use strict'
 
-    /*---tooltip---*/
+    /*---Tooltip---*/
     $('[data-toggle="tooltip"]').tooltip();
 
-    /*---stickey menu---*/
+    /*---Stickey Menu---*/
     var navbarSticky = $(".header-bottom");
     var stickyTop = navbarSticky.offset().top;
 
-    $(window).on('scroll',function() {
+    $(window).on('scroll', function() {
         var windowTop = $(window).scrollTop();
 
         if (stickyTop < windowTop) {
@@ -18,7 +18,7 @@ $(document).ready(function() {
         }
     });
 
-    /*---offcanvas menu---*/
+    /*---Offcanvas Menu---*/
     $('.navbar-toggler').on('click', function() {
         $('body').addClass('block');
         $('#header-navbar').addClass('show');
@@ -29,5 +29,21 @@ $(document).ready(function() {
         $('body').removeClass('block');
         $('#header-navbar').removeClass('show');
         $('.overlay').hide();
+    });
+
+    /*---Hero Slider---*/
+    var heroSlider = $('#section-1528354139490 .hero-slider');
+    heroSlider.slick({
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        dots: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        fade: true,
+        infinite: true,
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-prev"><i class="icofont icofont-long-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="icofont icofont-long-arrow-right"></i></button>',
     });
 });
