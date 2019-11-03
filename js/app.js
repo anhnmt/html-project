dda098b1a20c76684edbab32a5f80de27ec31088
Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
2019-11-03 12:34:27 +0700

    Update requirejs

--- js/app.js
+++ js/app.js
@@ -1,66 +1,78 @@
-$(document).ready(function() {
-    'use strict'
+requirejs.config({
+    paths: {
+        'jquery': './jquery.min',
+        'slick': './slick-slider/slick.min',
+        'lazyload': './lazyload/lazyload.min',
+        'bootstrap': './bootstrap/bootstrap.bundle.min',
+        'scrollUp': './scrollUp/jquery.scrollUp.min',
+    }
+});
 
-    /*---Slick-slider JS---*/
-    require(["js/slick-slider/slick.min.js"], function(slick) {
-        $('.hero-slider').slick({
-            arrows: false,
-            dots: true,
-            autoplay: true,
-            autoplaySpeed: 9000,
-            fade: true,
-            lazyLoad: 'ondemand',
+requirejs(['jquery'], function($) {
+    $(function() {
+        'use strict'
+
+        /*---Slick-slider JS---*/
+        require(['slick'], function(slick) {
+            $('.hero-slider').slick({
+                arrows: false,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 9000,
+                fade: true,
+                lazyLoad: 'ondemand',
+            });
         });
-    });
-    
-    /*---Lazyload---*/
-    require(["js/lazyload/lazyload.min.js"], function(LazyLoad) {
-        window.ll = new LazyLoad({
-            elements_selector: ".lazy",
+
+        /*---Lazyload---*/
+        require(['lazyload'], function(LazyLoad) {
+            window.ll = new LazyLoad({
+                elements_selector: ".lazy",
+            });
         });
-    });
 
-    /*---Bootstrap JS---*/
-    require(["js/bootstrap/bootstrap.bundle.min.js"], function() {
-        /*---Tooltip---*/
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-
-    /*---ScrollUp JS---*/
-    require(["js/scrollUp/jquery.scrollUp.min.js"], function(scrollUp) {
-        $.scrollUp({
-            easingType: 'linear',
-            scrollSpeed: 900,
-            animation: 'fade',
-            scrollText: '<i class="fas fa-arrow-up"></i>',
+        /*---Bootstrap JS---*/
+        require(['bootstrap'], function() {
+            /*---Tooltip---*/
+            $('[data-toggle="tooltip"]').tooltip();
         });
-    });
 
-    /*---Stickey Menu---*/
-    var navbarSticky = $(".header-bottom");
-    var stickyTop = navbarSticky.offset().top;
-    
-    /*---Scroll JS---*/
-    $(window).on('scroll', function() {
-        var windowTop = $(window).scrollTop();
+        /*---ScrollUp JS---*/
+        require(['scrollUp'], function(scrollUp) {
+            $.scrollUp({
+                easingType: 'linear',
+                scrollSpeed: 900,
+                animation: 'fade',
+                scrollText: '<i class="fas fa-arrow-up"></i>',
+            });
+        });
 
-        if (stickyTop < windowTop) {
-            navbarSticky.addClass("fixed-top");
-        } else {
-            navbarSticky.removeClass("fixed-top");
-        }
-    });
+        /*---Stickey Menu---*/
+        var navbarSticky = $(".header-bottom");
+        var stickyTop = navbarSticky.offset().top;
+        
+        /*---Scroll JS---*/
+        $(window).on('scroll', function() {
+            var windowTop = $(window).scrollTop();
 
-    /*---Offcanvas Menu---*/
-    $('.navbar-toggler').on('click', function() {
-        $('body').addClass('block');
-        $('#header-navbar').addClass('show');
-        $('.overlay').show();
-    });
+            if (stickyTop < windowTop) {
+                navbarSticky.addClass("fixed-top");
+            } else {
+                navbarSticky.removeClass("fixed-top");
+            }
+        });
 
-    $('.overlay').on('click', function() {
-        $('body').removeClass('block');
-        $('#header-navbar').removeClass('show');
-        $('.overlay').hide();
+        /*---Offcanvas Menu---*/
+        $('.navbar-toggler').on('click', function() {
+            $('body').addClass('block');
+            $('#header-navbar').addClass('show');
+            $('.overlay').show();
+        });
+
+        $('.overlay').on('click', function() {
+            $('body').removeClass('block');
+            $('#header-navbar').removeClass('show');
+            $('.overlay').hide();
+        });
     });
 });
