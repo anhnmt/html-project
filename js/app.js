412803e3082e1aaa88b83cb3c41ad86740b71d5f
Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
2019-11-06 20:49:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
+require.config({
+    deps: ['app'],
+    paths: {
+        jquery: 'jquery.min',
+        slick: 'slick/slick.min',
+        lazyload: 'lazyload/lazyload.min',
+        bootstrap: 'bootstrap/bootstrap.bundle.min',
+        scrollUp: 'scrollUp/jquery.scrollUp.min',
+        loginJS: 'myjs/login',
+        productJS: 'myjs/product',
+    },
+    shim: {
+        slick: {
+            deps: ["jquery"]
+        },
+        bootstrap: {
+            deps: ["jquery"]
+        },
+        scrollUp: {
+            deps: ["jquery"]
+        },
+        loginJS: {
+            deps: ["jquery"]
+        },
+    },
+});
+
 define(['jquery', 'bootstrap', 'loginJS', 'productJS'], function($) {
     'use strict'
 
