ed6bffffa8f27e0528fc2ed557b81b83e8e7b9be
Nguyen Manh Tuan Anh <42261776+xdorro@users.noreply.github.com>
2019-11-10 17:23:27 +0700

    Update cart & wishlist

--- js/app.js
+++ js/app.js
@@ -106,6 +106,47 @@ define(['jquery', 'bootstrap'], function($) {
         }
     });
 
+    $(".item-product").each(function(index) {
+        var id = $(this).data('id') ? $(this).data('id') : '';
+        var name = $(this).data('name') ? $(this).data('name') : '';
+        var cat = $(this).data('cat') ? $(this).data('cat') : '';
+        var img = $(this).data('img') ? $(this).data('img') : '';
+        var price = formatNumber($(this).data('price') ? $(this).data('price') : 0);
+        var old_price = $(this).data('old-price') ? formatNumber($(this).data('old-price')) : '';
+        var quantity = $(this).data('quantity') ? $(this).data('quantity') : 1;
+        // console.log(id, name, img, price, quantity);
+        $(this).html(`
+        <div class="item-image">
+            <a href="product.html" title="${name}">
+                <img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="${img}" alt="${name}">
+            </a>
+
+            <div class="item-wishlist">
+                <a class="add-wishlist" data-toggle="tooltip" title="Thích sản phẩm"><i class="far fa-heart"></i></a>
+                <a class="add-cart" data-toggle="tooltip" title="Thêm vào giỏ hàng"><i class="far fa-shopping-cart"></i></a>
+            </div>
+        </div>
+
+        <div class="item-content">
+            <div class="item-title">
+                <a href="product.html" title="${name}">
+                    <span class="cat">${cat}</span>
+                    <h5 class="title">${name}</h5>
+                </a>
+            </div>
+            <div class="item-price">
+                <h5 class="price">
+                    <span class="new">
+                        <span class="money">${price}</span>
+                    </span>
+                    <span class="old">
+                        <span class="money">${old_price}</span>
+                    </span>
+                </h5>
+            </div>
+        </div>`);
+    });
+
     /*---jQuery Validate---*/
     require(['jqueryValidate'], function(validate) {
         /*---Validate login---*/
@@ -204,51 +245,306 @@ define(['jquery', 'bootstrap'], function($) {
     });
 
     function formatNumber(num) {
-        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') + 'đ';
     }
 
-    function tongCong() {
-        var tong = 0;
+    // localStorage.products = localStorage.products ? localStorage.products : "";
+    // localStorage.wishlists = localStorage.wishlists ? localStorage.wishlists : "";
 
-        $("tbody .product-total").each(function(index) {
-            tong += parseFloat($(this).attr('data-price'));
-        });
 
-        $('.cart-price, .cart-total').html(formatNumber(tong) + 'đ');
-        return tong;
-    }
+    var WishlistManager = (function() {
+        var objToReturn = {};
 
-    $('.product-quantity input').bind('change', function() {
-        var parent = $(this).parent('td').parent('tr');
-        var val = parseFloat($(this).val());
-        var data_price = parseFloat(parent.data('product-price'));
-        var price = parseFloat(data_price * val);
-        parent.children('.product-total').attr('data-price', price).html(formatNumber(price) + 'đ');
-        tongCong();
-    });
-
-    $("tbody .product-remove").click(function(event) {
-        event.preventDefault();
-        if ($("tbody .product-remove").length === 1) {
-            $("tbody").html('<tr><td colspan="6">Không có gì cả...</td></tr>');
+        /*
+        PRIVATE
+        */
+        localStorage.wishlists = localStorage.wishlists ? localStorage.wishlists : "";
+        var getIndexOfWishlist = function(id) {
+            var wishlistIndex = -1;
+            var wishlists = getAllWishlists();
+            $.each(wishlists, function(index, value) {
+                if (value.id == id) {
+                    wishlistIndex = index;
+                    return;
+                }
+            });
+            return wishlistIndex;
+        }
+        var setAllWishlists = function(wishlists) {
+            localStorage.wishlists = JSON.stringify(wishlists);
+        }
+        var addWishlist = function(id, name, cat, price, image) {
+            var wishlists = getAllWishlists();
+            wishlists.push({
+                id: id,
+                name: name,
+                cat: cat,
+                price: price,
+                image: image
+            });
+            setAllWishlists(wishlists);
         }
 
-        $(this).parent('tr').remove();
-        tongCong();
-    });
+        /*
+        PUBLIC
+        */
+        var getAllWishlists = function() {
+            try {
+                var wishlists = JSON.parse(localStorage.wishlists);
+                return wishlists;
+            } catch (e) {
+                return [];
+            }
+        }
+        var setWishlist = function(id, name, cat, price, image) {
+            if (typeof id === "undefined") {
+                console.error("id required")
+                return false;
+            }
+            if (typeof name === "undefined") {
+                console.error("name required")
+                return false;
+            }
+            if (typeof image === "undefined") {
+                console.error("image required")
+                return false;
+            }
+            if (!$.isNumeric(price)) {
+                console.error("price is not a number")
+                return false;
+            }
+            cat = typeof cat === "undefined" ? "" : cat;
 
-    $('.product-quantity input').change();
-    tongCong();
+            if (!updateWishlist(id)) {
+                addWishlist(id, name, cat, price, image);
+            }
+        }
+        var clearWishlist = function() {
+            setAllWishlists([]);
+        }
+        var removeWishlist = function(id) {
+            var wishlists = getAllWishlists();
+            wishlists = $.grep(wishlists, function(value, index) {
+                return value.id != id;
+            });
+            setAllWishlists(wishlists);
+        }
+        var getTotalWishlist = function() {
+            var total = 0;
+            var wishlists = getAllWishlists();
+            $.each(wishlists, function(index, value) {
+                total += 1;
+            });
+            return total;
+        }
 
-    $("tbody .product-add, .add-cart").click(function(event) {
+        objToReturn.getAllWishlists = getAllWishlists;
+        objToReturn.setWishlist = setWishlist;
+        objToReturn.clearWishlist = clearWishlist;
+        objToReturn.removeWishlist = removeWishlist;
+        objToReturn.getTotalWishlist = getTotalWishlist;
+        return objToReturn;
+    }());
+
+    var ProductManager = (function() {
+        var objToReturn = {};
+
+        /*
+        PRIVATE
+        */
+        localStorage.products = localStorage.products ? localStorage.products : "";
+        var getIndexOfProduct = function(id) {
+            var productIndex = -1;
+            var products = getAllProducts();
+            $.each(products, function(index, value) {
+                if (value.id == id) {
+                    productIndex = index;
+                    return;
+                }
+            });
+            return productIndex;
+        }
+        var setAllProducts = function(products) {
+            localStorage.products = JSON.stringify(products);
+        }
+        var addProduct = function(id, name, cat, price, quantity, image) {
+            var products = getAllProducts();
+            products.push({
+                id: id,
+                name: name,
+                cat: cat,
+                price: price,
+                quantity: quantity,
+                image: image
+            });
+            setAllProducts(products);
+        }
+
+        /*
+        PUBLIC
+        */
+        var getAllProducts = function() {
+            try {
+                var products = JSON.parse(localStorage.products);
+                return products;
+            } catch (e) {
+                return [];
+            }
+        }
+        var updatePoduct = function(id, quantity) {
+            var productIndex = getIndexOfProduct(id);
+            if (productIndex < 0) {
+                return false;
+            }
+            var products = getAllProducts();
+            products[productIndex].quantity = typeof quantity === "undefined" ? products[productIndex].quantity * 1 + 1 : quantity;
+            setAllProducts(products);
+            return true;
+        }
+        var setProduct = function(id, name, cat, price, quantity, image) {
+            if (typeof id === "undefined") {
+                console.error("id required")
+                return false;
+            }
+            if (typeof name === "undefined") {
+                console.error("name required")
+                return false;
+            }
+            if (typeof image === "undefined") {
+                console.error("image required")
+                return false;
+            }
+            if (!$.isNumeric(price)) {
+                console.error("price is not a number")
+                return false;
+            }
+            if (!$.isNumeric(quantity)) {
+                console.error("quantity is not a number");
+                return false;
+            }
+            cat = typeof cat === "undefined" ? "" : cat;
+
+            if (!updatePoduct(id)) {
+                addProduct(id, name, cat, price, quantity, image);
+            }
+        }
+        var clearProduct = function() {
+            setAllProducts([]);
+        }
+        var removeProduct = function(id) {
+            var products = getAllProducts();
+            products = $.grep(products, function(value, index) {
+                return value.id != id;
+            });
+            setAllProducts(products);
+        }
+        var getTotalQuantityOfProduct = function() {
+            var total = 0;
+            var products = getAllProducts();
+            $.each(products, function(index, value) {
+                total += value.quantity * 1;
+            });
+            return total;
+        }
+
+        objToReturn.getAllProducts = getAllProducts;
+        objToReturn.updatePoduct = updatePoduct;
+        objToReturn.setProduct = setProduct;
+        objToReturn.clearProduct = clearProduct;
+        objToReturn.removeProduct = removeProduct;
+        objToReturn.getTotalQuantityOfProduct = getTotalQuantityOfProduct;
+        return objToReturn;
+    }());
+
+    $(".add-cart").click(function(event) {
         event.preventDefault();
-        var count = parseFloat($('.cart-quantity').html());
-        $('.cart-quantity').html(count + 1);
+        var $target = $(this).closest('.item-product');
+        var id = $target.data('id');
+        var name = $target.data('name');
+        var cat = $target.data('cat');
+        var price = $target.data('price');
+        var quantity = $target.data('quantity');
+        var image = $target.data('img');
+
+        ProductManager.setProduct(id, name, cat, price, quantity, image);
+        $('.cart-quantity').text(ProductManager.getTotalQuantityOfProduct());
     });
 
-    $(".add-wishlist").click(function(event) {
+    $(".product-add").click(function(event) {
         event.preventDefault();
-        var count = parseFloat($('.wishlist-quantity').html());
-        $('.wishlist-quantity').html(count + 1);
+        var $target = $(this).closest('tr');
+        var id = $target.data('id');
+        var name = $target.data('name');
+        var cat = $target.data('cat');
+        var price = $target.data('price');
+        var quantity = $target.data('quantity');
+        var image = $target.data('img');
+
+        ProductManager.setProduct(id, name, cat, price, quantity, image);
+        $('.cart-quantity').text(ProductManager.getTotalQuantityOfProduct());
     });
+
+    var drawTable = function() {
+        $('.table-cart tbody').empty();
+        var products = ProductManager.getAllProducts();
+        products.length ?
+            $.each(products, function() {
+                var total = formatNumber(this.quantity * this.price);
+                $('.table-cart tbody').append(`
+                <tr data-id="${this.id}" data-price="${this.price}">
+                    <td class="product-thumb">
+                        <a href="#"><img src="${this.image}" alt="${this.name}"></a>
+                    </td>
+                    <td class="product-name"><a href="#">${this.name}</a></td>
+                    <td class="product-price">${formatNumber(this.price)}</td>
+                    <td class="product-quantity"><input min="1" max="100" value="${this.quantity}" type="number"></td>
+                    <td class="product-total">${total}</td>
+                    <td class="product-remove"><a href="#"><i class="far fa-trash"></i></a></td>
+                </tr>`);
+            }) :
+            $('.table-cart tbody').append('<tr><td colspan="6">Không có gì cả ...</td></tr>');
+    }
+
+    $(document).on("input", ".product-quantity input", function() {
+        var $target = $(this).closest('tr');
+        var price = $target.data("price");
+        var id = $target.data("id");
+        var quantity = $(this).val();
+        var total = formatNumber(price * quantity);
+
+        $(this).parent("td").next(".product-total").text(total);
+        ProductManager.updatePoduct(id, quantity);
+        $('.cart-quantity').text(ProductManager.getTotalQuantityOfProduct());
+        var products = ProductManager.getAllProducts();
+        showGrandTotal(products);
+    });
+
+    $(document).on('click', "tbody .product-remove", function() {
+        var $target = $(this).closest('tr');
+        var id = $target.data("id");
+        $target.hide(500, function() {
+            ProductManager.removeProduct(id);
+            drawTable();
+            $('.cart-quantity').text(ProductManager.getTotalQuantityOfProduct());
+        });
+    });
+
+    var showGrandTotal = function(products) {
+        var total = 0;
+        $.each(products, function() {
+            total += this.quantity * this.price;
+        });
+        $(".cart-price, .cart-total").text(formatNumber(total));
+    }
+
+    var products = ProductManager.getAllProducts();
+    showGrandTotal(products);
+    $('.cart-quantity').text(ProductManager.getTotalQuantityOfProduct());
+    drawTable();
+
+    // $(".add-wishlist").click(function(event) {
+    //     event.preventDefault();
+    //     var count = parseFloat($('.wishlist-quantity').html());
+    //     $('.wishlist-quantity').html(count + 1);
+    // });
 });
